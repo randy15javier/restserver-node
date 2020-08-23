8cead891de00ecf03f2d2adc6c4e819c9d3d9df5
Randy <randyjavierh02@gmail.com>
2020-08-23 12:21:15 -0600

    Variable  de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/ProHealth';
 } else {
-    urlDB = 'mongodb+srv://randy15:realmadrid@cluster0.jiwcd.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
